72e5b4e4f645f4a4078f8571cd3a96ecba28d3d6
Owen Allen <owenallenaz@gmail.com>
2024-12-22 17:22:45 -0700

    fixing issues

--- src/scripts/notify.sh
+++ src/scripts/notify.sh
@@ -2,7 +2,7 @@
 env
 
 # Provide error if no webhook is set and error. Otherwise continue
-if [ -z "${webhook}" ]; then
+if [ -z "${WEBHOOK}" ]; then
     echo "NO MS TEAMS WEBHOOK SET"
     echo "Please input your MSTEAMS_WEBHOOK value either in the settings for this project, or as a parameter for this orb."
     exit 1
@@ -11,66 +11,66 @@ fi
 #If successful
 if [ "$MSTEAMS_BUILD_STATUS" = "success" ]; then
     #Skip if fail_only
-    if [ ${fail_only} = true ]; then
-    echo "The job completed successfully"
-    echo '"fail_only" is set to "true". No MS Teams notification sent.'
+    if [ "${FAIL_ONLY}" = true ]; then
+        echo "The job completed successfully"
+        echo '"fail_only" is set to "true". No MS Teams notification sent.'
     else
-    curl -X POST -H 'Content-type: application/json' \
-        --data \
-        "{ \
-        \"type\": \"message\", \
-        \"attachments\": [ \
-            { \
-            \"contentType\": \"application/vnd.microsoft.card.adaptive\", \
-            \"contentUrl\": null, \
-            \"content\": { \
-                \"$schema\": \"http://adaptivecards.io/schemas/adaptive-card.json\", \
-                \"type\": \"AdaptiveCard\", \
-                \"version\": \"1.2\", \
-                \"body\": [
+        curl -X POST -H 'Content-type: application/json' \
+            --data \
+            "{ \
+            \"type\": \"message\", \
+            \"attachments\": [ \
                 { \
-                    \"type\": \"TextBlock\", \
-                    \"size\": \"Medium\", \
-                    \"weight\": \"Bolder\", \
-                    \"color\": \"good\", \
-                    \"text\": \"${success_message}\", \
-                    \"wrap\": true \
-                }, \
-                { \
-                    \"type\": \"FactSet\", \
-                    \"facts\": [ \
+                \"contentType\": \"application/vnd.microsoft.card.adaptive\", \
+                \"contentUrl\": null, \
+                \"content\": { \
+                    \"\$schema\": \"http://adaptivecards.io/schemas/adaptive-card.json\", \
+                    \"type\": \"AdaptiveCard\", \
+                    \"version\": \"1.2\", \
+                    \"body\": [
                     { \
-                        \"title\": \"Project\", \
-                        \"value\": \"$CIRCLE_PROJECT_REPONAME\" \
+                        \"type\": \"TextBlock\", \
+                        \"size\": \"Medium\", \
+                        \"weight\": \"Bolder\", \
+                        \"color\": \"good\", \
+                        \"text\": \"${SUCCESS_MESSAGE}\", \
+                        \"wrap\": true \
                     }, \
                     { \
-                        \"title\": \"Branch\", \
-                        \"value\": \"$CIRCLE_BRANCH\" \
+                        \"type\": \"FactSet\", \
+                        \"facts\": [ \
+                        { \
+                            \"title\": \"Project\", \
+                            \"value\": \"$CIRCLE_PROJECT_REPONAME\" \
+                        }, \
+                        { \
+                            \"title\": \"Branch\", \
+                            \"value\": \"$CIRCLE_BRANCH\" \
+                        }, \
+                        { \
+                            \"title\": \"Pull Request Username\", \
+                            \"value\": \"$CIRCLE_PR_USERNAME\" \
+                        } \
+                        ] \
+                    } \
+                    ], \
+                    \"actions\": [ \
+                    { \
+                        \"type\": \"Action.OpenUrl\", \
+                        \"title\": \"View Job\", \
+                        \"url\": \"$CIRCLE_BUILD_URL\" \
                     }, \
                     { \
-                        \"title\": \"Pull Request Username\", \
-                        \"value\": \"$CIRCLE_PR_USERNAME\" \
+                        \"type\": \"Action.OpenUrl\", \
+                        \"title\": \"View PR\", \
+                        \"url\": \"$CIRCLE_PULL_REQUEST\" \
                     } \
                     ] \
                 } \
-                ], \
-                \"actions\": [ \
-                { \
-                    \"type\": \"Action.OpenUrl\", \
-                    \"title\": \"View Job\", \
-                    \"url\": \"$CIRCLE_BUILD_URL\" \
-                }, \
-                { \
-                    \"type\": \"Action.OpenUrl\", \
-                    \"title\": \"View PR\", \
-                    \"url\": \"$CIRCLE_PULL_REQUEST\" \
                 } \
-                ] \
-            } \
-            } \
-        ] \
-        }" "${webhook}"
-    echo "Job completed successfully. Alert sent."
+            ] \
+            }" "${WEBHOOK}"
+        echo "Job completed successfully. Alert sent."
     fi
 else
     #If Failed
@@ -83,7 +83,7 @@ else
             \"contentType\": \"application/vnd.microsoft.card.adaptive\", \
             \"contentUrl\": null, \
             \"content\": { \
-            \"$schema\": \"http://adaptivecards.io/schemas/adaptive-card.json\", \
+            \"\$schema\": \"http://adaptivecards.io/schemas/adaptive-card.json\", \
             \"type\": \"AdaptiveCard\", \
             \"version\": \"1.2\", \
             \"body\": [
@@ -92,7 +92,7 @@ else
                 \"size\": \"Medium\", \
                 \"weight\": \"Bolder\", \
                 \"color\": \"warning\", \
-                \"text\": \"${failure_message}\", \
+                \"text\": \"${FAILURE_MESSAGE}\", \
                 \"wrap\": true \
                 }, \
                 { \
@@ -123,6 +123,6 @@ else
             } \
         } \
         ] \
-    }" "${webhook}"
+    }" "${WEBHOOK}"
     echo "Job failed. Alert sent."
 fi
